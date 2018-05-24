f50ce055a3f54bd66d6ddb122274f0a79b61ffa4
perra <perra@172.27.148.190>
2018-05-24 17:34:11 +0200

    bicubic

---
+++ src/resampling/Bicubic.java
@@ -0,0 +1,36 @@
+package resampling;
+
+import DCT.Transform;
+
+public class Bicubic implements Transform {
+
+	@Override
+	public void setSourceData(Object src) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void calculate() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setKernel(double[][] kernel) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Object getResult() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
